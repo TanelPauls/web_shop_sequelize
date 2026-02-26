d52e5dcced011984e80c73d31afe2501e08d67ad
Tanel Pauls <paulstanel@protonmail.com>
2026-02-26 10:07:57 +0200

    products table made

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const sequelize = require('./utils/db.js');
-sequelize.authenticate().then(() => {
-    console.log('Connected to the database.');}).catch(err => {
-    console.error('Unable to connect to database', err);
-    });
 
+const models = require('./models/index.js');
+sequelize.models = models;
+
+sequelize.sync()
+  .then(() => {
+    console.log('Tabelid on loodud.');
+    app.listen(7013, "0.0.0.0", () => {
+      console.log('App is started at http://localhost:7013');
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 app.get('/', (req, res) => {
     res.json({message: 'web shop app'});
 });
 
-app.listen(7013, "0.0.0.0", ()=>{
-    console.log('App is started at http://localhost:7013')
-})
