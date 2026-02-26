a4405899ce9226e8891dc680a981128e27d6a797
Tanel Pauls <paulstanel@protonmail.com>
2026-02-26 11:56:05 +0200

    Create users table and relation to products table data

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 require("dotenv").config();
 
 const express = require('express');
-const productAdminRoutes = require('./routes/admin/products.js');
-const productRoutes = require('./routes/products.js');
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/admin', productAdminRoutes);
-app.use(productRoutes);
+
 
 const sequelize = require('./utils/db.js');
 
 const models = require('./models/index.js');
 sequelize.models = models;
 
-sequelize.sync()
+/* sequelize.sync({force: true})
   .then(() => {
     console.log('Tabelid on loodud.');
     app.listen(7013, "0.0.0.0", () => {
@@ -24,6 +22,37 @@ sequelize.sync()
     });
   })
   .catch((error) => {
+    console.error(error);
+  }); */
+app.use((req, res, next) => {
+    models.User.findByPk(1).then(user=>{
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+})
+
+const productAdminRoutes = require('./routes/admin/products.js');
+const productRoutes = require('./routes/products.js');
+
+app.use('/admin', productAdminRoutes);
+app.use(productRoutes);
+
+sequelize.sync({force: true}).then(()=> {
+    return models.User.findByPk(1);
+}).then(user => {
+    if(!user){
+        return models.User.create({
+            name: 'user',
+            email: 'user@local.com'
+        });
+    }
+    return user;
+}).then((user)=>{
+    console.log(user);
+    app.listen(7013, "0.0.0.0", () => {
+      console.log('App is started at http://localhost:7013');
+    });
+}).catch((error) => {
     console.error(error);
   });
 
